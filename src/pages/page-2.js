7d8d99df646ff65c69258d5b2df6d23a9cef22a1
Aman1401 <96782570+Aman1401@users.noreply.github.com>
2021-12-28 20:17:48 +0530

    updated second page

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -9,6 +9,9 @@ const SecondPage = () => (
     <Seo title="Page two" />
     <h1>Hi from the second page</h1>
     <p>Welcome to page 2</p>
+  <p>Gatsby is a creature of the modern web––the world of “JAMstack” architecture, static site generators and other newfangled concepts that make websites load faster and run more smoothly. In order to understand Gatsby, we need to pan out slightly and define some of the technologies it rests on, many of which have been shaping the web and web development for the last several years. 
+
+</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
